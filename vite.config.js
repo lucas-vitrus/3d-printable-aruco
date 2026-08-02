8ac8d550a6e4a9105169f8828b71a06391d89d6b
Lucas Cassiano <lucas@vitrus.ai>
2026-08-01 22:42:41 -0700

    Use repository path for static Pages assets

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
+    base: '/3d-printable-aruco/',
     plugins: [react()],
     server: { host: '127.0.0.1', port: 4178 },
     preview: { host: '127.0.0.1', port: 4178 },
